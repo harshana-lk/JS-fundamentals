ad2067090d8842b9f924d84a7cda758612d72476
Harshana Jayaweera <harshanajayaweerayt@gmail.com>
2023-02-26 22:22:39 +0530

    Correct way to declare a variable

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-// Variable Declaration---------------------------------------------
+// Variable Declaration(Correct Method)-----------------------------
 
 let nermo = "Harshana";
+let firstName = "Swarna";
 
 console.log(let);
 
 // -----------------------------------------------------------------
+
+// Variable Declaration(Wrong Methods)-----------------------------
+
+let 3olivia = "Jayaweera";
+
+let Harshana&Jayaweeara = "1234";
+
+// ----------------------------------------------------------------
