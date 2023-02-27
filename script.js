f6dfff0ced1f5301dd5209a980953987b4b125fe
harshana-lk <harshanajayaweerayt@gmail.com>
2023-02-27 15:43:01 +0530

    Data Types in JS

--- script.js
+++ script.js
@@ -3,14 +3,41 @@
 let nermo = "Harshana";
 let firstName = "Swarna";
 
-console.log(let);
+console.log(nermo);
 
 // -----------------------------------------------------------------
 
 // Variable Declaration(Wrong Methods)-----------------------------
 
 // let 3olivia = "Jayaweera";
-//
 // let Harshana&Jayaweeara = "1234";
 
-// ----------------------------------------------------------------
+// Data Types-------------------------------------------------------
+
+// ==> Boolean Value
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+// ==> String Value
+let java = "Amazing";
+console.log(typeof java);
+
+// ==> Number Value
+let numberType = 1234;
+console.log(typeof numberType);
+
+// ==> Undefined Value
+let UndefinedValue;
+console.log(typeof UndefinedValue);
+
+// ==> Null Value
+let nullValue = null;
+console.log(typeof nullValue);
+
+// ==> BigInt Value
+let bigIntValue = 14789656332144556332n;
+console.log(typeof bigIntValue);
+
+// ==> Dinamic Variable
+javascriptIsFun = "OG";
+console.log(typeof javascriptIsFun);
