6ec5553919f5d4cd9a278de17dd217f5f39bc878
harshana-lk <harshanajayaweerayt@gmail.com>
2023-02-27 22:11:39 +0530

    Operators

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Variable Declaration(Correct Method)-----------------------------
+// Variable Declaration(Correct Method)---------------------------------------------------------------------------------------
 
 let nermo = "Harshana";
 let firstName = "Swarna";
 
 console.log(nermo);
 
-// -----------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------
 
-// Variable Declaration(Wrong Methods)-----------------------------
+// Variable Declaration(Wrong Methods)-----------------------------------------------------------------------------------------
 
 // let 3olivia = "Jayaweera";
 // let Harshana&Jayaweeara = "1234";
 
-// Data Types-------------------------------------------------------
+// Data Types------------------------------------------------------------------------------------------------------------------
 
 // ==> Boolean Value
 let javascriptIsFun = true;
@@ -42,8 +42,7 @@ console.log(typeof bigIntValue);
 javascriptIsFun = "OG";
 console.log(typeof javascriptIsFun);
 
-
-// Let,Const,Var VAriable declaration
+// Let,Const,Var VAriable declaration----------------------------------------------------------------------------------------------
 
 // ==>Mutable Variable(LET)
 // we can change the value
@@ -59,3 +58,44 @@ sName = "Sewwandi";
 // we can't change the value
 const lName = "Jayaweera";
 lName = "Dissanayaka";
+
+// ==> Operators-------------------------------------------------------------------------------------------------------------------
+
+// Subtraction
+const now = 2023;
+const myAge = now - 2000;
+console.log(myAge);
+
+// Addition
+const salary = 50000.0;
+const nowSalary = salary + 5000;
+console.log(nowSalary);
+
+now += 200;
+console.log(now);
+
+now++;
+console.log(now);
+
+++now;
+console.log(now);
+
+// Multiplication & Division & Power
+console.log(nowSalary * 2, nowSalary / 2, 2 ** 3);
+
+nowSalary *= 10;
+console.log(nowSalary);
+
+nowSalary /= 10;
+console.log(nowSalary);
+
+// String Concat
+const first_name = "Harshana";
+const last_name = "Jayaweera";
+console.log(first_name + "" + last_name);
+
+// Comparison Operators
+const isNetSalary = nowSalary > salary;
+console.log(isNetSalary);
+
+
